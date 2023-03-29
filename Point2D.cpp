64cbdbc1a4a44a4a4f2712cebbf9807946a4479d
Richard Bachmann <102971493+rb-uf@users.noreply.github.com>
2023-03-29 10:12:24 -0400

    New Point2D stuff from Srikar.

--- Point2D.cpp
+++ Point2D.cpp
@@ -1,21 +1,38 @@
-#include <algorithm>
 #include "Point2D.h"
+#include <algorithm>
+#include <iostream>
+using namespace std;
 
 
 struct Point2D::Impl
 {
     Impl(){};
-    Impl(std::vector<SimplePoint2D> _pointCollection): pointCollection(move(_pointCollection)){};
-    std::vector<SimplePoint2D> pointCollection;
-};
 
+    Impl(vector<SimplePoint2D> _pointCollection): pointCollection(move(_pointCollection)){};
+
+    vector<SimplePoint2D> pointCollection;
+    
+    bool ordered;
+};
 Point2D::Point2D() {}
 
-Point2D::Point2D(std::vector<SimplePoint2D> _pointCollection):pimpl(new Impl())
+
+Point2D::Point2D(vector<SimplePoint2D> _pointCollection):pimpl(new Impl())
 {
     _pointCollection.erase(unique(_pointCollection.begin(), _pointCollection.end()), _pointCollection.end());
     sort(_pointCollection.begin(), _pointCollection.end());
     this->pimpl->pointCollection = _pointCollection;
+    this->pimpl->ordered = true;
+}
+
+Point2D::Point2D(vector<SimplePoint2D> _pointCollection, bool _ordered):pimpl(new Impl())
+{
+   this->pimpl->ordered = _ordered;
+   if(_ordered){
+        _pointCollection.erase(unique(_pointCollection.begin(), _pointCollection.end()), _pointCollection.end());
+        sort(_pointCollection.begin(), _pointCollection.end());
+   }
+   this->pimpl->pointCollection = _pointCollection;
 }
 
 Point2D::Point2D(Point2D const &sourcePoint2D): pimpl(new Impl(*sourcePoint2D.pimpl))
@@ -28,15 +45,19 @@ Point2D::Point2D(Point2D &&sourcePoint2D)
     sourcePoint2D.pimpl = nullptr;
 }
 
-Point2D::~Point2D() {}
+Point2D::~Point2D(){}
 
-Point2D::iterator Point2D::begin()
+int Point2D::count()
 {
-    return this->pimpl->pointCollection.begin();
+    return this->pimpl->pointCollection.size();
 }
 
-Point2D::iterator Point2D::end()
+Point2D::Iterator Point2D::begin()
 {
-    return this->pimpl->pointCollection.end();
+    return Iterator(&(this->pimpl->pointCollection)[0]);
 }
 
+Point2D::Iterator Point2D::end()
+{
+    return Iterator(&(this->pimpl->pointCollection)[this->pimpl->pointCollection.size()]);
+}
--- Point2D.h
+++ Point2D.h
@@ -1,28 +1,67 @@
 #ifndef POINT2D_H
 #define POINT2D_H
-
 #include <vector>
-#include <memory>
 #include "SimplePoint2D.h"
-#include "Line2D.h"
+#include <memory>
+using namespace std;
 
 
 class Point2D {
-private:
-    class Impl;
-    std::unique_ptr<Impl> pimpl;
+    private:
+        class Impl;
+        unique_ptr<Impl> pimpl;
+        
+    public:
+        struct Iterator{
+            public:
+                using iterator_category = input_iterator_tag;
+                using difference_type = ptrdiff_t;
+                using value_type = SimplePoint2D;
+                using pointer = SimplePoint2D*;
+                using reference = SimplePoint2D&;
 
-public:
-    Point2D();
-    Point2D(std::vector<SimplePoint2D> _pointCollection);
-    Point2D(Point2D const &sourcePoint2D);
-    Point2D(Point2D &&sourcePoint2D);
-    ~Point2D();
+                Iterator() {m_ptr = nullptr;}
+                Iterator(pointer ptr): m_ptr(ptr){}
 
-    typedef vector<SimplePoint2D>::iterator iterator;
-    iterator begin();
-    iterator end();
+                const reference operator*()const{return *m_ptr;}
+                const pointer operator->(){return m_ptr;}
+
+                Iterator& operator++()
+                {
+                    m_ptr++;
+                    return *this;
+                }
+                Iterator operator++(int)
+                {
+                    Iterator tmp = *this; 
+                    ++(*this);
+                    return tmp;
+                }
+
+                friend bool operator==(const Iterator& a, const Iterator& b)
+                {
+                    return a.m_ptr == b.m_ptr;
+                };
+                friend bool operator!=(const Iterator& a, const Iterator& b)
+                {
+                    return a.m_ptr != b.m_ptr;
+                };
+
+
+            private:
+                pointer m_ptr;
+
+        };
+
+        Point2D();
+        Point2D(vector<SimplePoint2D> _pointCollection, bool _ordered);
+        Point2D(vector<SimplePoint2D> _pointCollection);
+        Point2D(Point2D const &sourcePoint2D);
+        Point2D(Point2D &&sourcePoint2D);
+        ~Point2D();
+        int count();
+        Iterator begin();
+        Iterator end();
 };
-
 
 #endif //POINT2D_H
